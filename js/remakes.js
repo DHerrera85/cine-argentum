24faced20fe676c6cddb1358c74bea7616fa10e6
Daniel Herrera <74433631+DHerrera85@users.noreply.github.com>
2026-02-19 13:28:30 -0300

    Se guardan cambios

--- js/remakes.js
+++ js/remakes.js
@@ -1,4 +1,4 @@
-var dataVersion = '20260216-19';
+var dataVersion = '20260216-20';
 fetch('data.json?v=' + dataVersion, { cache: 'no-store' })
   .then(function (response) { return response.json(); })
   .then(function (data) {
